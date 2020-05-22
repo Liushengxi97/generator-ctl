2435ef33d71106d09ad894f645c2543cfde9b850
LiuShengxi <dahai469136241@163.com>
2020-05-22 14:22:28 +0800

    init

--- src/main/java/xyz/iahead/generatorctl/controller/GeneratorController.java
+++ src/main/java/xyz/iahead/generatorctl/controller/GeneratorController.java
@@ -1,23 +1,76 @@
 package xyz.iahead.generatorctl.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import xyz.iahead.generatorctl.model.Result;
+import xyz.iahead.generatorctl.utils.FileUtils;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/generator")
 public class GeneratorController {
 
+    @Value("${file.path}")
+    private String filePath;
+    @Value("${file.temp}")
+    private String fileTemp;
+
     @PostMapping("/ctl")
+    @ResponseBody
     public Result getCtl(List<MultipartFile> files){
+        List<String> filesUrl = new ArrayList<>();
+        for (MultipartFile file : files) {
+            //转成file,暂存在临时存储路径
+            String tempFile = null;
+            InputStream is = null;
+            FileOutputStream fileOutputStream = null;
+            try {
+                is = file.getInputStream();
+                String tempFilePath = filePath + fileTemp;
+                tempFile = tempFilePath + file.getOriginalFilename();
+                File tempPath = new File(tempFilePath);
+                if(!tempPath.exists()){
+                    tempPath.mkdirs();
+                }
+                fileOutputStream = new FileOutputStream(tempFile);
+                byte[] bytes = new byte[1024 * 1024];
+                while (is.read(bytes) > 0){
+                    fileOutputStream.write(bytes);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }finally {
+                try {
+                    is.close();
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            Map<String, Map<String, String>> fieldType = new HashMap<>();
+            Map<String, String> field1 = new HashMap<>();
+            field1.put("type", "DECIMAL EXTERNAL");
+            fieldType.put("SCMJ", field1);
+            Map<String, String> field2 = new HashMap<>();
+            field2.put("type", "DECIMAL EXTERNAL");
+            fieldType.put("SCMJM", field2);
+            String fileUrl = FileUtils.generatorCtl(new File(tempFile),"|", null, fieldType, filePath, null);
+            filesUrl.add(fileUrl);
+            //删除临时胜场的文件
+            new File(tempFile).delete();
+        }
 
-        return Result.ok(null);
+        return Result.ok(filesUrl);
     }
-
-
 
 }
--- src/main/java/xyz/iahead/generatorctl/utils/FileUtils.java
+++ src/main/java/xyz/iahead/generatorctl/utils/FileUtils.java
@@ -3,34 +3,79 @@ package xyz.iahead.generatorctl.utils;
 import org.springframework.util.StringUtils;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class FileUtils {
 
-    public static File generatorCtl(File file, String separator, String tableName, Map<String, String> fieldType, String filePath, String newFileName) {
-        FileReader fileReader;
+    private static List<String> SpecialChar = null;
+
+    static {
+        if (SpecialChar == null) {
+            SpecialChar = new ArrayList<>();
+            SpecialChar.add("|");
+            SpecialChar.add(".");
+            SpecialChar.add("\\");
+            SpecialChar.add("^");
+            SpecialChar.add("&");
+        }
+    }
+
+    /**
+     * @param file        生成ctl文件的依据
+     * @param separator   字段之间的分隔符
+     * @param tableName   需要入库的表名
+     * @param fieldType   {key:{type:"date", remark:"YYYY-MM-DD HH24:MI:SS"}}
+     * @param filePath    ctl文件存储位置
+     * @param newFileName ctl文件名
+     * @return
+     */
+    public static String generatorCtl(File file, String separator, String tableName, Map<String, Map<String, String>> fieldType, String filePath, String newFileName) {
+        UnicodeReader ur = null;
+        FileInputStream fis = null;
         String[] split = null;
         try {
-            fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            //去除BOM标识
+            fis = new FileInputStream(file);
+            ur = new UnicodeReader(fis, "utf-8");
+            BufferedReader bufferedReader = new BufferedReader(ur);
             String line = bufferedReader.readLine();
-            split = line.split(separator);
+            if (SpecialChar.contains(separator)) {
+                String newSeparator = "\\" + separator;
+                split = line.split(newSeparator);
+            } else {
+                split = line.split(separator);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                ur.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
+        //获取不带后缀的文件名 example.csv
         String oldFileName = file.getName();
-        //新文件名
+        int i = oldFileName.indexOf(".");
+        String realName = null;
+        if (i > 0) {
+            realName = oldFileName.substring(0, i);
+        }
+        //新文件名，如果位null，与旧文件同名
         if (StringUtils.isEmpty(newFileName)) {
-            newFileName = oldFileName + ".ctl";
+            newFileName = realName + ".ctl";
         }
         if (!newFileName.endsWith(".ctl")) {
             newFileName += ".ctl";
         }
-
-        File ctlFile = new File(filePath + newFileName);
+        String ctlFilePath = filePath + newFileName;
+        File ctlFile = new File(ctlFilePath);
         BufferedWriter writer = null;
         try {
             FileWriter fileWriter = new FileWriter(ctlFile);
@@ -42,74 +87,62 @@ public class FileUtils {
             writer.write("'");
             writer.newLine();
             writer.write("APPEND INTO TABLE ");
-            writer.write(tableName);
+            //若表名为空，以无后缀文件名作为表名
+            if (StringUtils.isEmpty(tableName)) {
+                writer.write(realName);
+            } else {
+                writer.write(tableName);
+            }
             writer.newLine();
             writer.write("FIELDS TERMINATED BY '");
             writer.write(separator);
             writer.write("'");
             writer.newLine();
-            writer.write("TRAILING NULLCOLS(");
+            writer.write("TRAILING NULLCOLS");
             writer.newLine();
+            writer.write("(");
             StringBuffer sb = new StringBuffer();
+
             //拼接字段
             for (String s : split) {
                 if (fieldType.containsKey(s)) {
-                    switch (fieldType.get(s)) {
-                        case "CHAR":
-
-                            break;
+                    Map<String, String> fieldInfo = fieldType.get(s);
+                    String type = fieldInfo.get("type");
+                    switch (type) {
                         case "DATE":
-
+                            String remark = fieldInfo.get("remark");
+                            sb.append(s);
+                            sb.append(' ');
+                            sb.append("\"");
+                            sb.append(remark);
+                            sb.append("\"");
                             break;
-                        case "DECIMAL EXTERNAL":
-
-                            break;
-                        case "DECIMAL":
-
-                            break;
-                        case "DOUBLE":
-
-                            break;
-                        case "FLOAT":
-
-                            break;
-                        case "FLOAT EXTERNAL":
-
-                            break;
-                        case "GRAPHIC EXTERNAL":
-
-                            break;
-                        case "INTEGER":
-
-                            break;
-                        case "INTEGER EXTERNAL":
-
-                            break;
-                        case "SMALLINT":
-
-                            break;
-                        case "VARCHAR":
-
-                            break;
-                        case "VARGRAPHIC":
-
-                            break;
-
+                        default:
+                            sb.append(s);
+                            sb.append(' ');
+                            sb.append(type);
+                            sb.append(',');
                     }
+                } else {
+                    sb.append(s);
+                    sb.append(",");
                 }
-                sb.append(s);
-                sb.append(",");
             }
             sb.deleteCharAt(sb.length() - 1);
+            sb.append(" TERMINATED BY WHITESPACE");
             writer.write(sb.toString());
             writer.write(")");
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
-
-
-        return null;
+        return ctlFilePath;
     }
-
 
 }
